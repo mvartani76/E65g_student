48123904e9cc14dfe7d3cc2ceb7eebc7096cb4a5
Mike Vartanian <mike.vartanian@gmail.com>
2017-03-15 01:22:50 -0400

    Added inspectable properties from problem 2. Also put Grid initialization inside drawRect() function. It looks like compiler chaned drawGrid to _ since it has not been used yet.

--- Assignment3/Assignment3/GridView.swift
+++ Assignment3/Assignment3/GridView.swift
@@ -8,14 +8,42 @@
 
 import UIKit
 
-class GridView: UIView {
-
-    /*
+@IBDesignable class GridView: UIView {
+    
+    @IBInspectable var size: Int = 20
+    @IBInspectable var livingColor = UIColor.init(
+                            red: CGFloat(0/255),
+                            green: CGFloat(0/255),
+                            blue: CGFloat(0/255),
+                            alpha: CGFloat(1.0))
+    @IBInspectable var emptyColor = UIColor.init(
+                            red: CGFloat(0/255),
+                            green: CGFloat(0/255),
+                            blue: CGFloat(0/255),
+                            alpha: CGFloat(1.0))
+    @IBInspectable var bornColor = UIColor.init(
+                            red: CGFloat(0/255),
+                            green: CGFloat(0/255),
+                            blue: CGFloat(0/255),
+                            alpha: CGFloat(1.0))
+    @IBInspectable var diedColor = UIColor.init(
+                            red: CGFloat(0/255),
+                            green: CGFloat(0/255),
+                            blue: CGFloat(0/255),
+                            alpha: CGFloat(1.0))
+    @IBInspectable var gridColor = UIColor.init(
+                            red: CGFloat(0/255),
+                            green: CGFloat(0/255),
+                            blue: CGFloat(0/255),
+                            alpha: CGFloat(1.0))
+    @IBInspectable var gridWidth = CGFloat(1.0)
+    
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
         // Drawing code
+        _ = Grid(self.size, self.size)
+        
     }
-    */
 
 }
