801474b473d679832338e17869e58652d93e30cd
Mike Vartanian <mike.vartanian@gmail.com>
2017-03-15 00:20:46 -0400

    Added a subclass of UIView called GridView.

---
+++ Assignment3/Assignment3/GridView.swift
@@ -0,0 +1,21 @@
+//
+//  GridView.swift
+//  Assignment3
+//
+//  Created by Michael Vartanian on 3/15/17.
+//  Copyright © 2017 Harvard Division of Continuing Education. All rights reserved.
+//
+
+import UIKit
+
+class GridView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
