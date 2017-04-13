37b635dd9edc591bf8dc29735c031e129e9d0cbb
Mike Vartanian <mike.vartanian@gmail.com>
2017-04-13 10:16:09 -0400

    Reduced timer from 2 to 1.

--- Assignment4/Assignment4/AppDelegate.swift
+++ Assignment4/Assignment4/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         // Added delay to show tab bar icon credits -- probably not a good idea to delay main thread though
-        sleep(2)
+        sleep(1)
         return true
     }
 
