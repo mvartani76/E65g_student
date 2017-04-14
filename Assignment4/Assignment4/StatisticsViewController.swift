428370b4c022e8c7ae2a88921b0137fa31c75235
Mike Vartanian <mike.vartanian@gmail.com>
2017-04-13 21:32:14 -0400

    Initial implementation of Q5 - StatisticsController. Still need to subscribe to the notifications.

--- Assignment4/Assignment4/StatisticsViewController.swift
+++ Assignment4/Assignment4/StatisticsViewController.swift
@@ -10,9 +10,52 @@ import UIKit
 
 class StatisticsViewController: UIViewController {
     
+    @IBOutlet weak var aliveCountLabel: UILabel!
+    @IBOutlet weak var bornCountLabel: UILabel!
+    @IBOutlet weak var deadCountLabel: UILabel!
+    @IBOutlet weak var emptyCountLabel: UILabel!
+    
+    var engine: StandardEngine!
+    
+    func updateCounts(withGrid: GridProtocol) {
+        
+        var aliveCount = 0
+        var bornCount = 0
+        var deadCount = 0
+        var emptyCount = 0
+        
+        (0 ..< withGrid.size.rows).forEach { i in
+            (0 ..< withGrid.size.cols).forEach { j in
+                
+                switch withGrid[j,i].description()
+                {
+                    case "alive":
+                        aliveCount = aliveCount + 1
+                    case "born":
+                        bornCount = bornCount + 1
+                    case "dead":
+                        deadCount = deadCount + 1
+                    default:
+                        emptyCount = emptyCount + 1
+                }
+            }
+        }
+        aliveCountLabel.text = String(aliveCount)
+        bornCountLabel.text = String(bornCount)
+        deadCountLabel.text = String(deadCount)
+        emptyCountLabel.text = String(emptyCount)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        engine = StandardEngine.shared()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        engine = StandardEngine.shared()
+        updateCounts(withGrid: engine.grid)
     }
     
     override func didReceiveMemoryWarning() {
