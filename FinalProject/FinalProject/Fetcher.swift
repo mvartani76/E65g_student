d4877597cab0bd7e2466a4baac019d6fdfbe0001
Mike Vartanian <mike.vartanian@gmail.com>
2017-04-27 19:40:23 -0400

    Changed LaunchScreen label from Assignment4 to Final Project.

---
+++ FinalProject/FinalProject/Fetcher.swift
@@ -0,0 +1,9 @@
+//
+//  Fetcher.swift
+//  FinalProject
+//
+//  Created by Michael Vartanian on 4/27/17.
+//  Copyright © 2017 Harvard Division of Continuing Education. All rights reserved.
+//
+
+import Foundation
