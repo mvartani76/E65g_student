0aea2ab12e1616e2acfc1a7505571a3f4588b74a
Mike Vartanian <mike.vartanian@gmail.com>
2017-05-03 14:16:40 -0400

    Removed code that saves grid values as string to text file.

--- FinalProject/FinalProject/Grid.swift
+++ FinalProject/FinalProject/Grid.swift
@@ -280,22 +280,6 @@ class StandardEngine: EngineProtocol {
                 }
             }
         }
-        
-        // Save data to file
-        let fileName = "Test"
-        let DocumentDirURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-        
-        let fileURL = DocumentDirURL.appendingPathComponent(fileName).appendingPathExtension("txt")
-        print("FilePath: \(fileURL.path)")
-        
-        let writeString = "Write this text to the fileURL as text in iOS using Swift"
-        do {
-            // Write to the file
-            try gridValues.write(to: fileURL, atomically: true, encoding: String.Encoding.utf8)
-        } catch let error as NSError {
-            print("Failed writing to URL: \(fileURL), Error: " + error.localizedDescription)
-        }
-        
     }
     
     // MARK: Engine toggleTimer
